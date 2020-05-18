27724348e1bb4e6679e708e280beeccf27d58c75
Al-amin Yususf <alaminyusuf131@gmail.com>
2020-05-18 23:56:54 +0100

    commit made

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const authPath = require('./routes/api/server');
-const app = express();
+const express = require('express')
+const authPath = require('./routes/api/server')
+const app = express()
 
-app.use('/', authPath);
+app.use('/', authPath)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log('Your server is ready'));
+app.listen(PORT, () => console.log(`Your server is ready on port ${PORT}`))
