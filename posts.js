f3cee2223ec0fd5637271339f1f409a573319165
Al-amin Yususf <alaminyusuf131@gmail.com>
2020-05-19 00:00:09 +0100

    made commit

--- posts.js
+++ posts.js
@@ -1,24 +1,30 @@
-const express = require('express');
+const express = require('express')
 
 const post = [
   {
     id: 1,
     name: 'Doe',
     age: 21,
-    email: 'doe@gmail.com'
+    email: 'doe@gmail.com',
   },
   {
     id: 2,
     name: 'Bink',
     age: 19,
-    email: 'bink@gmail.com'
+    email: 'bink@gmail.com',
   },
   {
     id: 3,
     name: 'John',
     age: 25,
-    email: 'john@gmail.com'
-  }
-];
+    email: 'john@gmail.com',
+  },
+  {
+    id: 4,
+    name: 'Jimmy',
+    age: 32,
+    email: 'jimmy@gmail.com',
+  },
+]
 
-module.exports = post;
+module.exports = post
